cc526fd5333b1d8d4b3cd11104a1ce98c8f2bf62
firut <firut@LAPTOP-83CGPGKK>
2025-11-27 21:06:24 +0300

    ограничение на ввод всех символов кроме цифр в поле контакты

--- wifi/EditPlaceWindow.xaml.cs
+++ wifi/EditPlaceWindow.xaml.cs
@@ -119,5 +119,27 @@ namespace wifi
             MessageBox.Show("Изменения успешно сохранены!");
             Close();
         }
+
+        private void ContactBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            // Удаляем все не-цифровые символы
+            var textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                string newText = new string(textBox.Text.Where(char.IsDigit).ToArray());
+                if (textBox.Text != newText)
+                {
+                    textBox.Text = newText;
+                    textBox.CaretIndex = newText.Length;
+                }
+            }
+        }
+
+        private void OnlyNum(object s, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.Text, 0);
+        }
     }
 }
+    
